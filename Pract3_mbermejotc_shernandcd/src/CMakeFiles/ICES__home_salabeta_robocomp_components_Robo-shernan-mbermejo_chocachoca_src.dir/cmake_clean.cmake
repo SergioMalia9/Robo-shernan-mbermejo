c7d013906021eb4462343ba4759c3f7edd8cc77b
SergioMalia9 <71331399+SergioMalia9@users.noreply.github.com>
2020-10-21 14:38:25 +0200

    nueva pract

---
+++ Pract3_mbermejotc_shernandcd/src/CMakeFiles/ICES__home_salabeta_robocomp_components_Robo-shernan-mbermejo_chocachoca_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_salabeta_robocomp_components_Robo-shernan-mbermejo_chocachoca_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
